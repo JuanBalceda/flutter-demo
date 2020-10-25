f2abaf0f066c4a20ce2b666a8c4ced7be88bc878
Juan Balceda <juanbalceda@gmail.com>
2020-10-25 22:39:02 +0100

    Added future example

---
+++ dart-language/10-futures.dart
@@ -0,0 +1,12 @@
+void main() {
+  print('method start');
+  var response = fakeHttpGet('http://api.com/resource')
+      .then((data) => print(data));
+  print('method end');
+}
+
+Future<String> fakeHttpGet(String url) {
+  return Future.delayed(Duration(seconds: 4), () { // Callback, Anonymous function
+    return 'Hello Future World';
+  });
+}
