9907131613f80cb33771f8d72db04e343cc5662f
Juan Balceda <juanbalceda@gmail.com>
2020-10-25 20:25:03 +0100

    Added hello world example

---
+++ dart-language/00-hello-world.dart
@@ -0,0 +1,17 @@
+void main() {
+  print('Hello World');
+
+  // This is a comment.
+
+  /*
+    This is 
+    a multiline
+    comment.
+   */
+
+  var name = 'Juan';
+  print('Hi, ' + name);
+
+  String lastName = 'Balceda';
+  print('Hi, $name $lastName');
+}
