293407b75c6457ee8d59a4c21126caaec5d58c52
Juan Balceda <juanbalceda@gmail.com>
2020-10-25 20:35:23 +0100

    Added basic data types example

---
+++ dart-language/01-data-types.dart
@@ -0,0 +1,20 @@
+void main() {
+  // Numbers
+  int employees = 10;
+  double pi = 3.141592;
+
+  print('employees: $employees');
+  print('pi: $pi');
+
+  // Strings
+  String name = 'Juan';
+  String lastName = "Balceda ' ";
+
+  print('first char:' + name[0]); // First characters
+  print('last char:' + name[name.length - 1]); // First characters
+
+  // Booleans
+  bool activated = true;
+  print(activated);
+  print(!activated);
+}
